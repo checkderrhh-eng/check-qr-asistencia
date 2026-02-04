ecaa3ab5b4c003c264a38b1ddf1e1ae2623e1590
checkderrhh-eng <checkderrhh@gmail.com>
2026-02-04 17:41:47 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'check-asistencia-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app-firebase.js',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/manifest.json'
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activación del Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Eliminando cache antiguo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepción de peticiones
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - devolver respuesta del cache
+        if (response) {
+          return response;
+        }
+        // Si no está en cache, hacer petición a la red
+        return fetch(event.request);
+      })
+  );
+});
